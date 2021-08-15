b6b71ed0b100f23834d1f3346356bf3acd98596f
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-08-15 16:36:27 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-var debounce = require('lodash.debounce');
+//var debounce = require('lodash.debounce');
+import countryCardTpl from '../templates/country-card.hbs';
+
+const URL = 'https://restcountries.eu/rest/v2/name';
+
+fetch(`${URL}/ukraine`)
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+    const markup = countryCardTpl(country);
+    console.log(markup);
+  })
+  .catch(error => {
+    console.log(error);
+  });
